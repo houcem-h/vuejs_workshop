10ee2ab808bf16b253a01f97361afd926eeacb47
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-27 17:39:12 +0100

    get contact list from api

--- contact-manager/src/components/ContactList.vue
+++ contact-manager/src/components/ContactList.vue
@@ -3,8 +3,24 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: 'ContactList',
+    data() {
+        return {
+            contacts: Array
+        }
+    },
+    created () {
+        axios.get('http://localhost:3000/contactsList')
+            .then(response => {
+                this.contacts = [...response.data];
+                console.log(this.contacts);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
 }
 </script>
 
