bbe85d5f07608a2ea80c0265ed7245427aa1bc27
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-27 16:11:04 +0100

    replace HelloWorld with ContactList

---
+++ contact-manager/src/components/ContactList.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Contact list component</h1>
+</template>
+
+<script>
+export default {
+    name: 'ContactList',
+}
+</script>
+
+<style>
+
+</style>
--- contact-manager/src/views/Home.vue
+++ contact-manager/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ContactList/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ContactList from '@/components/ContactList.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    ContactList
   }
 }
 </script>
